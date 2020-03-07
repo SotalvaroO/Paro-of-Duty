efdae83daeb583590d3c0b133db9c822a150885d
XxDirtyDannxX <33010627+SotalvaroO@users.noreply.github.com>
2020-03-07 00:35:50 -0500

    First Commit
    Personaje con movimiento básico en 2d, sin colisiones

---
+++ Paro of Duty/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public GameObject camera;
+    public GameObject cameraController;
+    public float speed = 10.0f;
+    Animator anim;
+    Rigidbody2D rb2d;
+    Vector2 mov;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mov = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+        
+        
+        /*Vector3 mov = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + mov, speed * Time.deltaTime);
+        camera.transform.position = Vector3.Lerp(camera.transform.position, cameraController.transform.position, 1.0f);
+        */
+
+    }
+    private void FixedUpdate()
+    {
+        rb2d.MovePosition(rb2d.position + mov * speed * Time.deltaTime);
+    }
+}
